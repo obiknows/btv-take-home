3b03d767a1d1910f2ac88ea5ca552c4e318c73f3
Obinna <obiknows88@gmail.com>
2023-01-11 23:27:24 +0200

    added some pages

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ type Props = {
   title?: string;
 };
 
-const Layout = ({ children, title = "BlackOak" }: Props) => (
+const Layout = ({ children, title = "BlackOakTV" }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
@@ -15,9 +15,8 @@ const Layout = ({ children, title = "BlackOak" }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     {/*  */}
-    <body className="bg-slate-900 min-h-[75rem]">
-      <script src="../../assets/js/hs.theme-appearance.js"></script>
-
+    <body className="bg-slate-900 min-h-[75rem] w-screen">
+      {/* Header */}
       <header className="sticky top-4 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
         <nav
           className="relative max-w-[85rem] w-full border  rounded-[36px] mx-2 py-3 px-4 md:flex md:items-center md:justify-between md:py-0 md:px-6 lg:px-8 xl:mx-auto bg-gray-800 border-gray-700"
@@ -29,7 +28,7 @@ const Layout = ({ children, title = "BlackOak" }: Props) => (
               href="#"
               aria-label="Brand"
             >
-              Brand
+              BlackOakTV
             </a>
             <div className="md:hidden">
               <button
@@ -47,7 +46,7 @@ const Layout = ({ children, title = "BlackOak" }: Props) => (
                   viewBox="0 0 16 16"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
                   />
                 </svg>
@@ -97,7 +96,7 @@ const Layout = ({ children, title = "BlackOak" }: Props) => (
               <div className="hs-dropdown [--strategy:static] md:[--strategy:fixed] [--adaptive:none] md:[--trigger:hover] md:py-4">
                 <button
                   type="button"
-                  className="flex items-center w-full text-gray-500 hover:text-gray-400 font-medium text-gray-400 hover:text-gray-500 "
+                  className="flex items-center w-full  font-medium text-gray-400 hover:text-gray-500 "
                 >
                   Dropdown
                   <svg
@@ -111,8 +110,8 @@ const Layout = ({ children, title = "BlackOak" }: Props) => (
                     <path
                       d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
                     ></path>
                   </svg>
                 </button>
@@ -141,8 +140,8 @@ const Layout = ({ children, title = "BlackOak" }: Props) => (
                         <path
                           d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
                           stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
+                          strokeWidth="2"
+                          strokeLinecap="round"
                         ></path>
                       </svg>
                     </button>
@@ -205,62 +204,7 @@ const Layout = ({ children, title = "BlackOak" }: Props) => (
         </nav>
       </header>
 
-      <main id="content" role="main">
-        <div className="max-w-[85rem] mx-auto pt-12 pb-10 px-4 sm:px-6 lg:px-8 md:pt-24">
-          <header className="max-w-3xl">
-            <p className="mb-2 text-sm font-semibold text-blue-600">
-              Starter Pages &amp; Examples
-            </p>
-            <h1 className="block text-2xl font-bold  sm:text-3xl text-white">
-              Sticky Floating Header using Tailwind CSS
-            </h1>
-            <p className="mt-2 text-lg  text-gray-400">
-              This is a floating navbar example using Tailwind CSS.
-            </p>
-            <div className="mt-5 flex flex-col items-center gap-2 sm:flex-row sm:gap-3">
-              <a
-                className="w-full sm:w-auto inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 focus:ring-offset-gray-800"
-                href="https://github.com/htmlstreamofficial/preline/tree/main/examples"
-                target="_blank"
-              >
-                <svg
-                  className="w-4 h-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                </svg>
-                Get the source code
-              </a>
-              <a
-                className="w-full sm:w-auto inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-blue-500 hover:text-blue-700 focus:outline-none focus:ring-2 ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm py-3 px-4 ring-offset-slate-900"
-                href="../../examples.html"
-              >
-                <svg
-                  className="w-2.5 h-2.5"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                >
-                  <path
-                    d="M11.2792 1.64001L5.63273 7.28646C5.43747 7.48172 5.43747 7.79831 5.63273 7.99357L11.2792 13.64"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  />
-                </svg>
-                Back to examples
-              </a>
-            </div>
-          </header>
-        </div>
-      </main>
-
-      {/* <script src="./@@node_modules/preline/dist/preline.js"></script> */}
+      {children}
     </body>
   </div>
 );
---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const LoginPage: NextPage = () => (
+  <Layout title="Log In | Black Oak TV">
+    <div className="w-screen h-full bg-green-500">a</div>
+  </Layout>
+);
+
+export default LoginPage;
