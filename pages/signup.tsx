7d6678b4d7dd0be2aa8c30d521374323d5444a4a
Obinna <obiknows88@gmail.com>
2023-01-13 14:24:55 +0200

    finessed another flow. more graphql

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,105 +1,157 @@
 import type { NextPage } from "next";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import Layout from "../components/Marketing";
 
-const SignUpPage: NextPage = () => (
-  <Layout title="Sign Up | Black Oak TV">
-    <section className="bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-white"
-        >
-          <img
-            className="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-            alt="logo"
-          />
-          BlackOak
-        </a>
-        <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
-              Create and account
-            </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
-              <div>
-                <label className="block mb-2 text-sm font-medium  text-white">
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                  placeholder="name@company.com"
-                />
+const SignUpPage: NextPage = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status == "authenticated") {
+    return (
+      <Layout title="Sign Up | Black Oak TV">
+        <section className="bg-gray-900">
+          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <a
+              href="#"
+              className="flex items-center mb-6 text-2xl font-semibold text-white"
+            >
+              <img
+                className="w-8 h-8 mr-2"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                alt="logo"
+              />
+              BlackOak
+            </a>
+            <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-xl font-bold  text-center mb-10 leading-tight tracking-tight  md:text-2xl text-white">
+                  You're already logged in
+                </h1>
+                <form className="space-y-4 md:space-y-6" action="#">
+                  <button
+                    onClick={() => router.push("/")}
+                    className="w-full text-white bg-blue-600 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800"
+                  >
+                    Go To Home
+                  </button>
+                </form>
               </div>
-              <div>
-                <label className="block mb-2 text-sm font-medium  text-white">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                />
-              </div>
-              <div>
-                <label className="block mb-2 text-sm font-medium  text-white">
-                  Confirm password
-                </label>
-                <input
-                  type="confirm-password"
-                  name="confirm-password"
-                  id="confirm-password"
-                  placeholder="••••••••"
-                  className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                />
-              </div>
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
+            </div>
+          </div>
+        </section>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout title="Sign Up | Black Oak TV">
+      <section className="bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a
+            href="#"
+            className="flex items-center mb-6 text-2xl font-semibold text-white"
+          >
+            <img
+              className="w-8 h-8 mr-2"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+              alt="logo"
+            />
+            BlackOak
+          </a>
+          <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
+                Create and account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label className="block mb-2 text-sm font-medium  text-white">
+                    Your email
+                  </label>
                   <input
-                    id="terms"
-                    aria-describedby="terms"
-                    type="checkbox"
-                    className="w-4 h-4 border rounded focus:ring-3 focus:ring-primary-300 bg-gray-700 border-gray-600 focus:ring-primary-600 ring-offset-gray-800"
+                    type="email"
+                    name="email"
+                    id="email"
+                    className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="name@company.com"
                   />
                 </div>
-                <div className="ml-3 text-sm">
-                  <label className="font-light  text-gray-300">
-                    I accept the{" "}
-                    <a
-                      className="font-medium text-primary-600 hover:underline text-primary-500"
-                      href="#"
-                    >
-                      Terms and Conditions
-                    </a>
+                <div>
+                  <label className="block mb-2 text-sm font-medium  text-white">
+                    Password
                   </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                  />
                 </div>
-              </div>
-              <button
-                type="submit"
-                className="w-full text-white bg-blue-600 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800"
-              >
-                Create an account
-              </button>
-              <p className="text-sm font-light  text-gray-400">
-                Already have an account?{" "}
-                <a
-                  href="#"
-                  className="font-medium text-primary-600 hover:underline text-primary-500"
+                <div>
+                  <label className="block mb-2 text-sm font-medium  text-white">
+                    Confirm password
+                  </label>
+                  <input
+                    type="confirm-password"
+                    name="confirm-password"
+                    id="confirm-password"
+                    placeholder="••••••••"
+                    className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                  />
+                </div>
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      className="w-4 h-4 border rounded focus:ring-3 focus:ring-primary-300 bg-gray-700 border-gray-600 focus:ring-primary-600 ring-offset-gray-800"
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label className="font-light  text-gray-300">
+                      I accept the{" "}
+                      <a
+                        className="font-medium text-primary-600 hover:underline text-primary-500"
+                        href="#"
+                      >
+                        Terms and Conditions
+                      </a>
+                    </label>
+                  </div>
+                </div>
+                <button
+                  onClick={() => {
+                    signIn(
+                      "credentials",
+                      {},
+                      { username: "admin", password: "password" }
+                    ).then(() => {
+                      router.push("/");
+                    });
+                  }}
+                  className="w-full text-white bg-blue-600 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800"
                 >
-                  Login here
-                </a>
-              </p>
-            </form>
+                  Create an account
+                </button>
+                <p className="text-sm font-light  text-gray-400">
+                  Already have an account?{" "}
+                  <a
+                    href="/login"
+                    className="font-medium text-primary-600 hover:underline text-primary-500"
+                  >
+                    Login here
+                  </a>
+                </p>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  </Layout>
-);
+      </section>
+    </Layout>
+  );
+};
 
 export default SignUpPage;
